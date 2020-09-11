349b0115182e0321343a936a5e7d58a6c9a8c0f1
Rony Mathews <autosivadasan97@gmail.com>
2020-09-11 16:32:37 +0530

    Successfully made a test paymongo api call locally with request module

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Paymongo = require("paymongo");
+const request = require("request");
+// const Paymongo = require("paymongo");
 
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded( { extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 //Get request to root route
-app.get("/", function(req, res){
-  console.log("Ofcourse nothing will be displayed! It is under development!");
+app.get("/", function(req, res) {
+  console.log("Ofcourse nothing will be displayed! It is under development! But take this since you are persistent!");
+  const options = {
+    method: 'GET',
+    url: 'https://api.paymongo.com/v1/webhooks',
+    headers: {
+      authorization: 'Basic c2tfdGVzdF83WlFpTlVUd2QyYWpZWnl1RW9zZGtzVVg6'
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+    console.log(response);
+  });
 });
 
 
 //Server startup message
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Paymongo server is currently under development! No results would come up in browsers.");
 });
